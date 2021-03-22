30afd40695c708d36d5b82ed7dffbf0b748f97b3
raywel51 <kittayawat.aong@gmail.com>
2021-03-22 17:30:49 +0700

    Update user_product.php

--- include/user_product.php
+++ include/user_product.php
@@ -1,41 +1,40 @@
-<style>#l {height: 250px;width: 50%;}
-</style>
+<style>#l {height: 250px;width: 50%;}</style>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="css/myStyle.css">
 <div class="w3-white" id="tour">
 
 
 <div class="w3-container" >
-<div class="w3-main w3-content w3-center "">
-<div class="w3-row-padding w3-padding-16 w3-center" id="item">
+<div class="w3-main w3-content w3-center">
+<div class="w3-row-padding-padding w3-padding-0 w3-center " id="item">
 
 
 <?php 
 
-require_once ('connection/connect.php');
+include('connection\connect.php');
 
 $query = "SELECT * FROM product";
 
 $result = mysqli_query ($conn,$query);
 
-while ($row = mysqli_fetch_array ($result, MYSQLI_ASSOC)) {
+while ($db = mysqli_fetch_array ($result, MYSQLI_ASSOC)) {
 
 	
-	// Display each record.
-	if ($image = @getimagesize ("img/product/{$row['product_img']}")) {
-		echo "   
+	if ($image = @getimagesize ("img/product/{$db['product_img']}")) { ?>
 
 
-    <div class='w3-third w3-card w3-section w3-margin-bottom w3-container w3-white'>
+    <div class='w3-col s4 w3-card w3-section w3-margin-bottom w3-container w3-white w3-col'>
     
-		            <img id='l' src=\"img/product/{$row['product_img']}\" alt=\"{$row['product_name']}\"   style='width:100%; height:20% class='w3-hover-opacity'>
-		            <p><b>{$row['product_name']}</b></p>
-		            <p class='w3-left'><b>฿{$row['product_price']}</b></p>
-			    <p class='w3-right'><a class='w3-button w3-round-large w3-black'href =\"add_cart.php?pid={$row['product_id']}\">Add to cart</a></p><br/>
+		<img id='l' src="img/product/<?php echo $db['product_img'];?>" alt="<?php echo $db['product_name'];?>"   style='width:150px; height:150px; class='w3-hover-opacity'>
+		<p><b><?php echo $db['product_name'];?></b></p>
+		<p class='w3-left'><b>฿<?php echo $db['product_price'];?></b></p>
+		<p class='w3-right'><a class='w3-button w3-round-large w3-black'href ="add_cart.php?pid=<?php echo $db['product_id'];?>\">Add to cart</a></p><br/>
 
-  
-		            </div>";	
-	} else {
+  	</div>
+
+
+	<?php } else {
 		echo "<div align=\"center\">No image available."; 
 	}
 	
